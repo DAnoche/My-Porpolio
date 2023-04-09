7dc30800b544810e684e92bb5c891fa96a9c32e9
Tomeee <diether.noche@gmail.com>
2023-04-09 21:20:28 +0800

    Add some contents to Hero Section

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -3,7 +3,31 @@ import React from "react";
 function HeroSection(props) {
   return (
     <section className={`hero mt-5 ${props.theme}`}>
-      <div className="container">{/* Something Here */}</div>
+      <div className="container">
+        <div className="container-lg">
+          <div className="row justify-content-center align-items-center text-center text-md-start">
+            <div className="col-md-5 text-center text-md-start">
+              <h1>
+                <div className="display-2">Front End Developer</div>
+              </h1>
+              <p className="lead my-4">
+                Blending creativity and technical expertise to build engaging
+                web applications.
+              </p>
+              <a className="btn btn-primary btn-lg" href="#pricing">
+                Download CV
+              </a>
+            </div>
+            <div className="col-md-5 text-center d-none d-md-block">
+              <img
+                className="image-fluid"
+                src={"./assets/hero-pic.svg"}
+                alt="Web Developer"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
