8db0dd9900afc6cb9d20fc645a3056cf4a10ec7d
Tomeee <diether.noche@gmail.com>
2023-04-09 12:28:19 +0800

    Make the navbar responsive using bootstrap

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -4,38 +4,48 @@ import "./navbar.css";
 function Navbar() {
   return (
     <>
-      <div className="container mt-3">
-        <header className="d-flex flex-wrap justify-content-center py-3 mb-4">
-          <a
-            href="/"
-            className="nav-logo d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none fs-4"
-          >
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container">
+          <a href="/" className="navbar-brand">
             DAN.
           </a>
-          <ul className="nav-ul-dark nav">
-            <li className="nav-item">
-              <a href="#About-me" className="nav-link">
-                ABOUT ME
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="#Project" className="nav-link">
-                PROJECTS
-              </a>
-            </li>
-            <li className="nav-item me-2">
-              <a href="#Contact" className="nav-link">
-                CONTACT
-              </a>
-            </li>
-            <li className="nav-item d-flex justify-contents-center align-items-center">
-              <span className="nav-theme-icon ">
-                <i className="fas fa-moon"></i>
-              </span>
-            </li>
-          </ul>
-        </header>
-      </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a href="#About-me" className="nav-link">
+                  ABOUT ME
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#Project" className="nav-link">
+                  PROJECTS
+                </a>
+              </li>
+              <li className="nav-item me-2">
+                <a href="#Contact" className="nav-link">
+                  CONTACT
+                </a>
+              </li>
+              <li className="nav-item d-flex justify-contents-center align-items-center">
+                <span className="nav-theme-icon ">
+                  <i className="fas fa-moon"></i>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
