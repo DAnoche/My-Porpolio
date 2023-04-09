42e2b9d3f5ac49cd4d548e4968c3a84b039e3155
Tomeee <diether.noche@gmail.com>
2023-04-09 13:03:38 +0800

    make the navbar responsive

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -1,52 +1,76 @@
-import React from "react";
-import "./navbar.css";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faTimes,
+  faMoon,
+  faSun,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
+  const [collapsed, setCollapsed] = useState(true);
+  const [theme, setTheme] = useState("dark");
+
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <a href="/" className="navbar-brand">
-            DAN.
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a href="#About-me" className="nav-link">
-                  ABOUT ME
-                </a>
-              </li>
-              <li className="nav-item">
-                <a href="#Project" className="nav-link">
-                  PROJECTS
-                </a>
-              </li>
-              <li className="nav-item me-2">
-                <a href="#Contact" className="nav-link">
-                  CONTACT
-                </a>
-              </li>
-              <li className="nav-item d-flex justify-contents-center align-items-center">
-                <span className="nav-theme-icon ">
-                  <i className="fas fa-moon"></i>
-                </span>
-              </li>
-            </ul>
-          </div>
+    <nav className={`navbar navbar-expand-md mt-3 ${theme}`}>
+      <div className="container">
+        <a href="/" className="nav-logo navbar-brand">
+          DAN
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={toggleCollapse}
+        >
+          <span className="nav-toggler">
+            {collapsed ? (
+              <FontAwesomeIcon icon={faBars} />
+            ) : (
+              <FontAwesomeIcon icon={faTimes} />
+            )}
+          </span>
+        </button>
+        <div
+          className={`collapse navbar-collapse ${collapsed ? "" : "show"}`}
+          id="navbarNav"
+        >
+          <ul className="nav-ul-dark navbar-nav ms-auto">
+            <li className="nav-item">
+              <a href="#About-me" className="nav-link">
+                ABOUT ME
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#Project" className="nav-link">
+                PROJECTS
+              </a>
+            </li>
+            <li className="nav-item me-2">
+              <a href="#Contact" className="nav-link">
+                CONTACT
+              </a>
+            </li>
+            <li
+              className="nav-item d-flex justify-contents-start align-items-center"
+              onClick={toggleTheme}
+            >
+              <span className="nav-theme-icon">
+                <FontAwesomeIcon icon={theme === "dark" ? faMoon : faSun} />
+                &nbsp;{theme === "dark" ? "Dark" : "Light"}
+              </span>
+            </li>
+          </ul>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
 
