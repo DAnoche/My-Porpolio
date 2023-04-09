46d98fa2c51d9e8ddca93fbe7f89fee09872e10e
Tomeee <diether.noche@gmail.com>
2023-04-09 23:24:13 +0800

    Make the hero section more responsive

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -12,7 +12,7 @@ function HeroSection(props) {
     <section className={`hero ${props.theme}`}>
       <div className="container p-3">
         <div className="row justify-content-center align-items-center text-center text-md-start">
-          <div className="col-md-5 text-center text-md-start">
+          <div className="col-md-5 text-center text-lg-start">
             <h1 className="hero-header display-2 fw-bold">
               <Typewriter
                 onInit={(typewriter) => {
@@ -31,34 +31,37 @@ function HeroSection(props) {
               Blending creativity and technical expertise to build engaging web
               applications.
             </p>
-            <div className="hero-links d-flex justify-contents-center align-items-center">
+            <div className="hero-links d-flex justify-contents-center align-items-center flex-lg-row flex-column">
               <button className="hero-btn me-3">
                 <i className="fas fa-download hero-download" /> Download CV
               </button>
-              <a href="https://www.facebook.com/DietherNoche" target="_blank">
-                <FontAwesomeIcon
-                  icon={["fab", "facebook"]}
-                  className="hero-social-links me-3"
-                />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/diether-noche-010356145/"
-                target="_blank"
-              >
-                <FontAwesomeIcon
-                  icon={["fab", "linkedin"]}
-                  className="hero-social-links me-3"
-                />
-              </a>
-              <a href="https://github.com/DAnoche" target="_blank">
-                <FontAwesomeIcon
-                  icon={["fab", "github"]}
-                  className="hero-social-links me-3"
-                />
-              </a>
+              <div className="d-lg-none mb-3"></div>
+              <div className="d-md-flex justify-content-center align-items-center">
+                <a href="https://www.facebook.com/DietherNoche" target="_blank">
+                  <FontAwesomeIcon
+                    icon={["fab", "facebook"]}
+                    className="hero-social-links me-3"
+                  />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/diether-noche-010356145/"
+                  target="_blank"
+                >
+                  <FontAwesomeIcon
+                    icon={["fab", "linkedin"]}
+                    className="hero-social-links me-3"
+                  />
+                </a>
+                <a href="https://github.com/DAnoche" target="_blank">
+                  <FontAwesomeIcon
+                    icon={["fab", "github"]}
+                    className="hero-social-links me-3"
+                  />
+                </a>
+              </div>
             </div>
           </div>
-          <div className="col-md-5 text-center d-none d-md-block">
+          <div className="col-md-5 text-center d-none d-lg-block">
             <img
               className="image-fluid"
               src={"./assets/hero-pic.svg"}
