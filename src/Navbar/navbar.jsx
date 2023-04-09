18d6d5b52b3935b3fe64a05aa2c17052f9ce42b1
Tomeee <diether.noche@gmail.com>
2023-04-09 11:16:29 +0800

    Change navbar text color on hover

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -4,37 +4,34 @@ import "./navbar.css";
 function Navbar() {
   return (
     <>
-      <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
+      <div className="container mt-3">
+        <header className="d-flex flex-wrap justify-content-center py-3 mb-4">
           <a
             href="/"
-            class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-light text-decoration-none fs-4"
+            className="nav-logo d-flex align-items-center mb-3 mb-md-0 me-md-auto text-decoration-none fs-4"
           >
             DAN.
           </a>
-          <ul class="nav-ul-dark nav nav-pills">
-            <li class="nav-item">
-              <a href="#About-me" class="nav-link">
+          <ul className="nav-ul-dark nav">
+            <li className="nav-item">
+              <a href="#About-me" className="nav-link">
                 ABOUT ME
               </a>
             </li>
-            <li class="nav-item">
-              <a href="#Project" class="nav-link">
+            <li className="nav-item">
+              <a href="#Project" className="nav-link">
                 PROJECTS
               </a>
             </li>
-            <li class="nav-item">
-              <a href="#Contact" class="nav-link">
+            <li className="nav-item me-2">
+              <a href="#Contact" className="nav-link">
                 CONTACT
               </a>
             </li>
-            <li class="nav-item me-2">
-              <button class="btn btn-primary">RESUME</button>
-            </li>
-            <li class="nav-item">
-              <button class="btn btn-secondary">
-                <i class="fas fa-moon"></i>
-              </button>
+            <li className="nav-item d-flex justify-contents-center align-items-center">
+              <span className="nav-theme-icon ">
+                <i className="fas fa-moon"></i>
+              </span>
             </li>
           </ul>
         </header>
