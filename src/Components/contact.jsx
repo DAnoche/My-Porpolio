0051d10f41b353b7abbaefaa2777a4400dbd7cef
Tomeee <diether.noche@gmail.com>
2023-04-12 00:46:33 +0800

    Install email js and add boilerplate

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 
 function ContactSection(props) {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <section id="Contact" className={`contact py-5 ${props.theme}`}>
       <div className="about-intro container">
@@ -23,8 +45,8 @@ function ContactSection(props) {
         <div className="row justify-content-center align-items-center text-center text-md-start py-5">
           <div className="col-md-5 text-center d-none d-lg-block">
             <motion.img
-              initial={{ y: "100%", opacity: 0 }}
-              whileInView={{ y: 0, opacity: 1 }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
               transition={{ duration: 0.3 }}
               whileHover={{ rotate: 5 }}
               className="image-fluid"
@@ -36,7 +58,17 @@ function ContactSection(props) {
             initial={{ y: "100%", opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             className="col-md-5 text-center text-lg-start"
-          ></motion.div>
+          >
+            <form ref={form} onSubmit={sendEmail}>
+              <label>Name</label>
+              <input type="text" name="user_name" />
+              <label>Email</label>
+              <input type="email" name="user_email" />
+              <label>Message</label>
+              <textarea name="message" />
+              <input type="submit" value="Send" />
+            </form>
+          </motion.div>
         </div>
       </div>
     </section>
